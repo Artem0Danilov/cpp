7f3c28d28e9adfdeeaf1fa9b48d19daa6c10e74f
Vasya <vasyasilkin@gmail.com>
2025-05-29 23:45:25 +0300

    ConsoleApplication19/

--- ConsoleApplication19/ConsoleApplication19.cpp
+++ ConsoleApplication19/ConsoleApplication19.cpp
@@ -1,6 +1,11 @@
 ﻿#include <iostream>
+#include "piret.h"
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+   Pirate first ("Vasya", 50);
+   first.give_Money();
+   first.give_away_Money();
+   first.GetInfo();
 }
---
+++ ConsoleApplication19/piret.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "piret.h"
+
+using namespace std;
+
+Pirate::Pirate(const string& name, int age):
+     name_(name), age_(age), amoutofGold_(0) {};
+    int Pirate:: give_Money(){
+        cout << "Add how money you want take\n";
+        int number;
+        cin >> number;
+        amoutofGold_+= number;
+        return amoutofGold_;
+
+    }
+    int Pirate::  give_away_Money(){
+        cout << "Add how money you want give away\n";
+        int number;
+        cin >> number;
+        amoutofGold_-= number;
+        return amoutofGold_;
+
+    }
+    void Pirate:: GetInfo(){
+        cout << "---------------------\n";
+        cout <<  "Name: " << name_ << endl;
+        cout << "Gold: " << amoutofGold_ << endl;
+        cout << "Age: " << age_ << endl;
+        cout << "----------------------\n";
+
+    }
+
+
+    
